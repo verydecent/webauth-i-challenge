1abb8048756d3269ea6eb28382684eb487416704
verydecent <wnjhwng@gmail.com>
2019-06-29 10:05:41 -0700

    Implements GET users

--- users/users-router.js
+++ users/users-router.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
+const Database = require('./users-model');
+
+router.get('/users', (req, res) => {
+  Database.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(404).json({ message: "Could not retrieve users from database" });
+    });
+});
+
 module.exports = router;
