581f47a55e46cde561090a8719f5c741e8541af0
verydecent <wnjhwng@gmail.com>
2019-06-29 10:11:22 -0700

    Import and use user-router.js into server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
 
+const usersRouter = require('../users/users-router');
+
 server.use(express.json());
+server.use('/api', userRouter);
 
 module.exports = server;
