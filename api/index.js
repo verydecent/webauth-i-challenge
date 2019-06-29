9a6c5d4061e93d0e85af0b54d12b9315e1457653
verydecent <wnjhwng@gmail.com>
2019-06-29 10:19:17 -0700

    Imports and uses authRouter in server

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const server = express();
 
 const usersRouter = require('../users/users-router');
+const authRouter = require('../auth/auth-router');
 
 server.use(express.json());
+
 server.use('/api', usersRouter);
+server.use('/api', authRouter);
 
 module.exports = server;
